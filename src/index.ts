00c09236f41abac02b9ead4d1c279247dcb794c3
Alex Profir <alex_profir@yahoo.com>
2022-02-08 20:04:29 +0200

    added random methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,49 @@
-export const Sum = (a: number, b: number) => a + b; 
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
+
+export const Sum = (a: number, b: number) => a + b;
+
+let app: FirebaseApp | null = null;
+export function initialize(...props: Parameters<typeof initializeApp>) {
+    app = initializeApp(...props);
+
+}
+function assertAppExists(app: any): asserts app is FirebaseApp {
+    if (!app) {
+        throw new Error("Firebase app not initialized");
+    }
+}
+export function createUser(email: string, password: string) {
+    assertAppExists(app);
+
+    const auth = getAuth();
+    return createUserWithEmailAndPassword(auth, email, password);
+}
+export function loginWithEmail(email: string, password: string) {
+    assertAppExists(app);
+
+    const auth = getAuth();
+    return signInWithEmailAndPassword(auth, email, password);
+}
+export async function loginWithGoogle() {
+    assertAppExists(app);
+
+    const auth = getAuth();
+    const firestore = getFirestore();
+
+    const googleAuthProvider = new GoogleAuthProvider();
+
+    const data = await signInWithPopup(auth, googleAuthProvider);
+
+    const firstName = data.user?.displayName?.split(" ")[0];
+    const lastName = data.user?.displayName?.split(" ")[1];
+
+    const cityRef = doc(firestore, 'users', data.user?.uid);
+    await setDoc(cityRef, {
+        firstName,
+        lastName
+    }, { merge: true });
+    return data;
+}
