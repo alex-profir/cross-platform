c9a6643d67ff9d7b9a192635ebe7e251a78ea152
Alex Profir <alex_profir@yahoo.com>
2022-02-08 21:42:10 +0200

    added analytics

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { FirebaseApp, initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { doc, setDoc } from "firebase/firestore";
-
+import { logEvent, getAnalytics } from "firebase/analytics";
 export const Sum = (a: number, b: number) => a + b;
 
 let app: FirebaseApp | null = null;
@@ -15,22 +15,33 @@ function assertAppExists(app: any): asserts app is FirebaseApp {
         throw new Error("Firebase app not initialized");
     }
 }
-export function createUser(email: string, password: string) {
+export async function createUser(email: string, password: string) {
     assertAppExists(app);
 
     const auth = getAuth();
-    return createUserWithEmailAndPassword(auth, email, password);
+    const analytics = getAnalytics();
+
+    const user = await createUserWithEmailAndPassword(auth, email, password);
+    const operation = user.operationType
+    logEvent(analytics, "createUser", { operation });
+    return user;
 }
-export function loginWithEmail(email: string, password: string) {
+export async function loginWithEmail(email: string, password: string) {
     assertAppExists(app);
 
     const auth = getAuth();
-    return signInWithEmailAndPassword(auth, email, password);
+    const analytics = getAnalytics();
+
+    const user = await signInWithEmailAndPassword(auth, email, password);
+    const operation = user.operationType
+    logEvent(analytics, "login", { operation });
+    return user;
 }
 export async function loginWithGoogle() {
     assertAppExists(app);
 
     const auth = getAuth();
+    const analytics = getAnalytics();
     const firestore = getFirestore();
 
     const googleAuthProvider = new GoogleAuthProvider();
@@ -40,6 +51,7 @@ export async function loginWithGoogle() {
     const firstName = data.user?.displayName?.split(" ")[0];
     const lastName = data.user?.displayName?.split(" ")[1];
 
+    logEvent(analytics, "login", { operation: "google_login" });
     const cityRef = doc(firestore, 'users', data.user?.uid);
     await setDoc(cityRef, {
         firstName,
