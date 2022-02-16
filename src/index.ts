2671e2214fa2ebfb265b9e37af63acfbb7a58300
Alex Profir <alex_profir@yahoo.com>
2022-02-16 12:14:00 +0200

    removed firebase as a dependency

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,6 @@
-import firebase from "firebase/app";
-// import auth from "firebase/auth";
-import "firebase/auth";
-import "firebase/functions";
-import "firebase/firestore";
+import type firebase from "firebase"
 
 export const Sum = (a: number, b: number) => a + b;
-
-let app: firebase.app.App | null = null;
-
-export function initialize(...props: Parameters<typeof firebase.initializeApp>) {
-    app = firebase.initializeApp(...props);
-
-}
 
 type Nullable<T> = {
     [P in keyof T]: T[P] | null
@@ -68,50 +57,6 @@ export async function getDocWithProvider(docPath: string) {
         onChange: ref.onSnapshot.bind(ref),
     }
 
-}
-
-function assertAppExists(app: any): asserts app is firebase.app.App {
-    if (!app) {
-        throw new Error("Firebase app not initialized");
-    }
-}
-export async function createUser(email: string, password: string) {
-    assertAppExists(app);
-
-    return await app.auth().createUserWithEmailAndPassword(email, password);
-}
-export async function loginWithEmail(email: string, password: string) {
-    assertAppExists(app);
-
-
-    return await app.auth().signInWithEmailAndPassword(email, password);
-}
-
-export async function logout() {
-    assertAppExists(app);
-
-    return app.auth().signOut();
-}
-
-export async function getData(docPath: string) {
-    assertAppExists(app);
-    const db = app.firestore();
-    const ref = db.doc(docPath);
-
-    const data = await ref.get();
-    return {
-        data: data.data(),
-        onChange: ref.onSnapshot.bind(ref),
-        // unsubscribe
-    }
-}
-
-export async function functionCall(functionName: string, body: object) {
-    assertAppExists(app);
-
-
-    const call = app.functions().httpsCallable(functionName);
-    return (await call.call(body)).data;
 }
 
 export async function functionCallWithProvider(functionName: string, body?: any) {
